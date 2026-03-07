30468954cb630833ff0a38ad4eea3221e79ddaf1
Parallax10 <131243633+Parallax10@users.noreply.github.com>
2026-03-07 19:16:14 +0100

    5

--- src/app/api/articulos/route.js
+++ src/app/api/articulos/route.js
@@ -1,5 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
-import { NextResponse } from 'next/server';
 const supabaseUrl = 'https://zshyyemefdwmlqcbxjrv.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpzaHl5ZW1lZmR3bWxxY2J4anJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3MzQxNjMsImV4cCI6MjA4ODMxMDE2M30.Ms-bChRS2byX4gfZ0MeBSXlx4kV1LL58TN_Ok7BOST8';
 const supabase = createClient(supabaseUrl, supabaseKey);
@@ -22,24 +21,16 @@ export async function GET(request) {
 export async function DELETE(request) {
     const body=await request.json()
     const idArticulo=body.id
-    
     const {data:deleteData,error}=await supabase
     .from("articulos")
     .delete()
     .eq("id",idArticulo)
-
-    if (error){ 
-        return new Response(JSON.stringify(error),{
-            status:400,
-            headers:{"Content-Type":"application/json"},
-        });
+    if(error){
+        return new Response(JSON.stringify(error),{status:400});
     }
-
-    return new Response(
-        JSON.stringify({success:"Articulo eliminado"}),
-        {status:200},
-    );
+    return new Response(JSON.stringify({success:"Articulo eliminado"}),{status:200});
 }
+
 
 
 
